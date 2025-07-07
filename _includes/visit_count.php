6a2ae5932bbe54a305016739baf7454c280e5cf1
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2025-07-07 19:52:09 +0300

    main: visit count added

--- _includes/visit_count.php
+++ _includes/visit_count.php
@@ -2,8 +2,8 @@
 
 try {
     $HOME = strlen($_SERVER['DOCUMENT_ROOT']) != 0 ? $_SERVER['DOCUMENT_ROOT'] : "";
-    include "$HOME/php/LogVisit.php";
-    LogVisit::logVisit();
+    include "$HOME/php/VisitCount.php";
+    VisitCount::updateCount();
 }
 catch(Throwable $exception) {}
 
